42b4edfb5d298896aade06f09d157f6db92c85f9
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-19 22:19:10 +0000

    add feature

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [tarea, setTarea] = useState("");
+	const [listaDeTareas, setListaDeTareas] = useState([""]);
+	console.log(typeof listaDeTareas)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="">
+			<form onSubmit={(event) => {
+				event.preventDefault()
+				setListaDeTareas([tarea, ...listaDeTareas])
+				setTarea("")
+			}}>
+				<input type="text"
+					onChange={(event) => {
+						setTarea(event.target.value)
+
+					}}
+					value={tarea} className="form-control"
+					placeholder="qlqmuchachos"
+					aria-label="Username" aria-describedby="basic-addon1" />
+				<button
+					type="submit"
+
+				>enter</button>
+			</form>
+			<div className="">
+				<ul className="list-group">
+					{listaDeTareas.map((tareaAMostrar) => {
+						return(
+						<li className="list-group-item">
+							{tareaAMostrar}
+						</li>
+						)
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
